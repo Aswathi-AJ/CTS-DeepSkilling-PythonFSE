7c9ad3d9bd9d67dff68e899989210998a29d99e4
Aswathi <aswathiaj23@gmail.com>
2026-06-27 20:48:15 +0530

    Completed Hands-On 03: Django REST Framework APIs and ViewSets

--- PythonBackendFrameworks/Aswathi-AJ/coursemanager/coursemanager/settings.py
+++ PythonBackendFrameworks/Aswathi-AJ/coursemanager/coursemanager/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'courses.apps.CoursesConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- PythonBackendFrameworks/Aswathi-AJ/coursemanager/courses/urls.py
+++ PythonBackendFrameworks/Aswathi-AJ/coursemanager/courses/urls.py
@@ -1,6 +1,18 @@
-from django.urls import path
-from .views import hello_view
+from rest_framework.routers import DefaultRouter
+from .views import CourseViewSet,StudentViewSet,EnrollmentViewSet
 
-urlpatterns = [
-    path("hello/", hello_view, name="hello"),
-]
+router = DefaultRouter()
+router.register(
+    r'courses',
+    CourseViewSet
+)
+router.register(
+    r'students',
+    StudentViewSet
+)
+
+router.register(
+    r'enrollments',
+    EnrollmentViewSet
+)
+urlpatterns = router.urls
